a2efe2f9a3144790d9038ee881d3744c7bba00ff
manasvin <manasvin@users.noreply.github.com>
2021-11-29 00:11:34 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,34 @@
+#!/bin/sh
 
 
 if [ -z "${SFTP_USERNAME}" ]; then
-  echo "Need username as argument ..."
-  exit 1
+    providedusername="sftpuser"
+else
+    providedusername="${SFTP_USERNAME}"
 fi
 
-providedusername="${SFTP_USERNAME}"
 
-adduser -D -s /sbin/nologin $providedusername
+adduser -h /home/$providedusername -D -s /sbin/nologin $providedusername
 echo "$providedusername:ftp123" | chpasswd
-#chown -R root:root "/home/$providedusername"
-#chmod 755 /home/sftpuser 
+chown -R root:root "/home/$providedusername"
+chmod 755 "/home/$providedusername"
 
-cd /home/$providedusername 
-ln -s datafeed/folder1 folder1 
+cd /home/$providedusername
+ln -s datafeed/folder1 folder1
 ln -s datafeed/folder2 folder2
 
-userKeysQueuedDir="/home/$user/.ssh/keys"
+userKeysQueuedDir="/home/$providedusername/.ssh/keys"
+
 if [ -d "$userKeysQueuedDir" ]; then
-    userKeysAllowedFileTmp="$(mktemp)"
-    userKeysAllowedFile="/home/$user/.ssh/authorized_keys"
+    
+    userKeysAllowedFile="/home/$providedusername/.ssh/authorized_keys"
 
     for publickey in "$userKeysQueuedDir"/*; do
         cat "$publickey" >> "$userKeysAllowedFile"
     done
 
-    chown "$uid" "$userKeysAllowedFile"
+    chown "$providedusername" "$userKeysAllowedFile"
     chmod 600 "$userKeysAllowedFile"
 fi
-
 
 exec "$@"
